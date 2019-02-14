247c09a54ca97e0eb5b16d9fce0e9b1acc5535ce
Sivan Duijn <sivanduijn>
2019-02-14 16:36:54 +0100

    simple something

---
+++ sandy_iframe/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            message = HtmlEncoder.Default.Encode(message);
+            await Clients.Caller.SendAsync("ReceiveMessage", "Hoi, dit is een response message van de server op je bericht: " + message);
+        }
+    }
+}
---
+++ sandy_iframe/wwwroot/js/Chat.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = msg;
+    var p = document.createElement("p");
+    p.textContent = encodedMsg;
+    var messagesList = document.getElementById("messagesList");
+    messagesList.appendChild(p);
+    messagesList.parentElement.scrollTop = messagesList.parentElement.scrollHeight;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var inputBox = document.getElementById("messageInput");
+    var message = inputBox.value;
+    inputBox.value = "";
+    connection.invoke("SendMessage", message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
