950751ed03549775954b670c282fe28f5e4f66db
Sivan Duijn <sivanduijn@gmail.com>
2019-02-15 17:14:01 +0000

    added styling

--- sandy_iframe/wwwroot/js/Chat.js
+++ sandy_iframe/wwwroot/js/Chat.js
@@ -7,12 +7,8 @@ document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = msg;
-    var p = document.createElement("p");
-    p.textContent = encodedMsg;
-    var messagesList = document.getElementById("messagesList");
-    messagesList.appendChild(p);
-    messagesList.parentElement.scrollTop = messagesList.parentElement.scrollHeight;
+    createSandyMessage(msg);
+
 });
 
 connection.start().then(function () {
@@ -24,9 +20,66 @@ connection.start().then(function () {
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var inputBox = document.getElementById("messageInput");
     var message = inputBox.value;
-    inputBox.value = "";
-    connection.invoke("SendMessage", message).catch(function (err) {
-        return console.error(err.toString());
-    });
+    if (message.length > 0) {
+      inputBox.value = "";
+      createUserMessage(message, "user");
+      connection.invoke("SendMessage", message).catch(function (err) {
+          return console.error(err.toString());
+      });
+    }
     event.preventDefault();
 });
+
+var messagesList;
+var datetime;
+
+$(document).ready(function() {
+  messagesList = document.getElementById("messagesList");
+  datetime = new Date();
+
+  var input = document.getElementById("messageInput");
+  input.addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("sendButton").click();
+    }
+  });
+});
+
+function createUserMessage(text) {
+  var msgBox = createAndAppend("div", messagesList, "user_msg");
+  var msg = createAndAppend("div", msgBox, "sent_msg");
+  createAndAppend("p", msg, "", text);
+  createAndAppend("span", msg, "date_time", datetime.toLocaleTimeString());
+
+  messagesList.parentElement.scrollTop = messagesList.parentElement.scrollHeight;
+}
+function createSandyMessage(text) {
+  var msgBox = createAndAppend("div", messagesList, "sandy_incoming_msg");
+
+  var img_div = createAndAppend("div", msgBox, "sandy_msg_img");
+  var img = createAndAppend("img", img_div);
+  img.src = "https://ptetutorials.com/images/user-profile.png";
+  img.alt = "Sandy";
+
+  var msg = createAndAppend("div", msgBox, "sandy_msg");
+  var msg_withd = createAndAppend("div", msg, "sandy_withd_msg");
+  createAndAppend("p", msg_withd, "", text);
+  createAndAppend("span", msg_withd, "date_time", datetime.toLocaleTimeString());
+
+  messagesList.parentElement.scrollTop = messagesList.parentElement.scrollHeight;
+}
+
+function createElement(type, className = "", html = "") {     // returns a new html elemnt with specified type and attributes
+  var item = document.createElement(type);
+  if (className.length > 0)
+    item.className = className;
+  item.innerHTML = html;
+  return item;
+}
+
+function createAndAppend(type, parent, className = "", html = "") {  // create a new html element and add it to the parent, with specified attributes
+  var item = createElement(type, className, html);
+  parent.append(item);
+  return item;                                            // still return the item, because we than we can still add items to that
+}
