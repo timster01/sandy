37a239a82833fc4fecf833097d929113b6a93f9c
Sivan Duijn <sivanduijn@gmail.com>
2019-04-04 18:11:36 +0100

    met testproject, nog zonder selenium

--- SandyTest/Properties/AssemblyInfo.cs
+++ SandyTest/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("sandyTest")]
+[assembly: AssemblyTitle("SandyTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("sandyTest")]
+[assembly: AssemblyProduct("SandyTest")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("6d4a0c59-4c71-4650-bdac-a6fcb62173d6")]
+[assembly: Guid("38cf97d5-3690-4fad-8d1c-647aeb17b92f")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
