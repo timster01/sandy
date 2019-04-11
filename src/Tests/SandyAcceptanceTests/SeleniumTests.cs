c5b26fc2ef0337d17fb08e40d37cdbaee994542a
Sivan Duijn <sivanduijn>
2019-04-11 09:40:17 +0200

    new project structure and xUnit tests met selenium

---
+++ src/Tests/SandyAcceptanceTests/SeleniumTests.cs
@@ -0,0 +1,75 @@
+using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Firefox;   
+using OpenQA.Selenium.Chrome;    
+using OpenQA.Selenium.IE;
+
+public class SeleniumTests : IDisposable
+{
+    private IWebDriver driver;
+
+    public SeleniumTests()
+    {
+        string appURL = "https://sandylinux.azurewebsites.net";
+
+        driver = new FirefoxDriver();
+        driver.Navigate().GoToUrl(appURL);
+
+        WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
+        wait.Until(drvr => drvr.FindElements(By.ClassName("sandy_incoming_msg")).Count == 2);
+    }
+
+    [Fact]
+    public void GreetingsTest()
+    {
+        string[] msgs = GetResponse("hoi");
+
+        string expected = "Hallo!";
+        string actual = msgs[0];
+        Assert.Equal(expected, actual);
+
+        UnknownQuestionTest();
+    }
+
+    [Fact]
+    public void UnknownQuestionTest()
+    {
+        string[] msgs = GetResponse("Hoe heet ik?", 4);
+
+        string expected = "Wil je een email adres opgeven?";
+        string actual = msgs[3];
+        Assert.Equal(expected, actual);
+    }
+
+    private string[] GetResponse(string message, int nExpectedResponseMsgs = 1)
+    {
+        int sandyMsgs = driver.FindElements(By.ClassName("sandy_incoming_msg")).Count;
+        int totalMsgsExpected = sandyMsgs + nExpectedResponseMsgs;
+
+        driver.FindElement(By.Id("messageInput")).SendKeys(message);
+        driver.FindElement(By.Id("sendButton")).Click();
+
+        WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
+        wait.Until(drvr => drvr.FindElements(By.ClassName("sandy_incoming_msg")).Count >= totalMsgsExpected);
+
+        // test if we received the expected amount of response messages
+        int totalMsgs = driver.FindElements(By.ClassName("sandy_incoming_msg")).Count;
+        Assert.Equal(totalMsgs, totalMsgsExpected);
+
+        string[] responses = new string[nExpectedResponseMsgs];
+        for (int i = 0; i < responses.Length; i++)
+        {
+            IWebElement rWE = driver.FindElements(By.ClassName("sandy_incoming_msg"))[sandyMsgs + i];
+            responses[i] = rWE.FindElement(By.XPath("div[2]/div/p")).Text;
+        }
+
+        return responses;
+    }
+
+    public void Dispose()
+    {
+        driver.Quit();
+    }
+}
