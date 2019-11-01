d517950af090886b44815a078da11bc1c0f28752
timster01 <timroggen@hotmail.nl>
2019-11-01 22:28:27 +0100

    Removed redundant port env var

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
     node_env: process.env.NODE_ENV,
-    port: process.env.PORT,
     slackWebAppHook: process.env.slackWebAppHook,
     slackAppBotAccessToken: process.env.slackAppBotAccessToken,
     slackAppId: process.env.slackAppId,
